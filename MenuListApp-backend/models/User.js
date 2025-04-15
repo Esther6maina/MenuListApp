a6caa5e6fb86a36a67efcd39e0fdb329f4380f90
Esther Maina <estanyambura99@gmail.com>
2025-04-15 09:17:45 +0300

    Add email support to user registration and login

--- MenuListApp-backend/models/User.js
+++ MenuListApp-backend/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true }, // Add email field
   password: { type: String, required: true },
 });
 
